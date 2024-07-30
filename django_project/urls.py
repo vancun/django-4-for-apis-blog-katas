5e802822f3ac28d61fcbeb4ae202df6a5ec850b5
DESKTOP-8ISDEH9\ivang <ivan.georgiev@gmail.com>
2024-07-30 22:26:02 +0300

    swagger ui as separate kata

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,28 +1,8 @@
 # django_project/urls.py
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Blog API",
-      default_version='v1',
-      description="API documentation for the Blog application",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@example.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
     path('api/', include('posts.urls')),
 ]
